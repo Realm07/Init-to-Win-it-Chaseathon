974757ef08b4af6a5f75eeb4c03ad31783bb3c27
Realm07 <52783069+Realm07@users.noreply.github.com>
2025-08-24 09:17:42 +0530

    Day 1 Mini Task

---
+++ Day 1.js
@@ -0,0 +1,11 @@
+const student = {
+    name: "Shees",
+    age: 20,
+    course: "Computer Science & Engineering",
+    marks: 91
+};
+
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
+console.log("Marks:", student.marks);
